5721c1d2d2bc142c419f26611fc85f8cd13b6be8
Diama1 <danimahoro75@gmail.com>
2020-02-18 13:10:14 +0200

    chore/171210908(unit test): setting up unit test
    - install all dependecies for tests
    - create the first unit test
    - add test coverage badge

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const api = express();
 
-api.get('/', (req, res) => res.send('Welcome to my Portfolio'));
+api.get('/', (req, res) => res.json({ status: 200, message: 'Welcome To Portfolio' }));
 
 export default api;
